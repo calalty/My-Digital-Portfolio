f9d905b3b7510d32f86d85c3267f832de940ee4f
Callum Alton <callum.alton33@gmail.com>
2020-05-21 17:15:07 +0100

    updated react digital portfolio with css

--- src/Portfolio/About.js
+++ src/Portfolio/About.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import Projects from './Projects'
 
 const About = () => {
     return (
         <div>
         <div className='main-content'>
+            <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+
             <div className='names'>
             <h1 className='first-name'>Callum</h1>
             <h2 className='second-name'>Alton</h2>
             </div>
             <h3 className='job-title'>Junior Software Developer</h3>
-        </div>
+            </div>
         <div className='info'>
             <div className='about-me'><h1>About...</h1>
             <p>Born in Cheshire, I had been a student at Sandbach School for 8 years. Not knowing what i wanted to do for my future career, and studying my A-levels for a year and a half
@@ -18,7 +32,7 @@ const About = () => {
                 I had realised that an apprenticeship with a Tech bias where i could show some creative flare would be my preffered route. I have a genuine passion for technology, and i seem to have the ability
                 to pick up technical subjects quickly and run with them, this is why becoming a Software Developer has become my dream.</p></div>
         </div>
-        <button>Projects</button>
+        <button>My Projects </button>
         </div>
     )
 }
--- src/Portfolio/Projects.js
+++ src/Portfolio/Projects.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Projects = () => {
     return (
         <div>
-            <h1>Projects</h1>
         </div>
     )
 }
